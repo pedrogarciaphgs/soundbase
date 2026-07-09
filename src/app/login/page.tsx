bc227d3257e9689a58ebe536b223f2b9f70a6d2e
Pedro Henrique Garcia <pedrogarciaphg@outlook.com>
2026-07-09 11:49:24 +0100

    chore: enable docker hot reload

---
+++ src/app/login/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import type React from "react";
+import { signIn } from "next-auth/react";
+
+async function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const result = await signIn("credentials", {
+    email,
+    password,
+    redirect: false,
+  });
+
+  console.log(result);
+}
+
+export default function LoginPage() {
+  return (
+    <main>
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="email" />
+        <input name="password" type="password" />
+        <button type="submit">Entrar </button>
+      </form>
+    </main>
+  );
+}
